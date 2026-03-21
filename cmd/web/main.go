7f82d0b0d483d4d6fa78bc4cec66d1da03b32618
Arques <bhuvaneshkpb@gmail.com>
2026-03-21 23:40:54 +0530

    [Bhuvanesh][Feat]:Initialized project and setup requirements

---
+++ cmd/web/main.go
@@ -0,0 +1,19 @@
+package main
+
+const webport = "80"
+
+func main(){
+	// connnect to postgres database
+
+	// create session
+
+	// create channel
+
+	// create waitgroup
+
+	// set up application configuration
+
+	// set up mail
+
+	// listen to web connection
+}
